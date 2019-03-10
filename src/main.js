08f3400f91375b136017b997294fb841979ba35d
dingpeiyang <dpy1515925189@gmail.com>
2019-03-10 15:11:10 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+//导入自己的路由模块
 import router from './router'
-
 // 导入mui 的样式
 import './lib/mui/css/mui.min.css'
-
+//导入mui 扩展样式(扩展字体库还要拷贝 .ttf文件)
+import './lib/mui/css/icons-extra.css'
 // 按需导入mint-ui
-import { Header } from 'mint-ui';
+import { Header,Swipe,SwipeItem } from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name,Swipe);
+Vue.component(SwipeItem.name,SwipeItem);
 
+// 使用vue-resource获取数据
 
-
-
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 
 
 Vue.config.productionTip = false
@@ -22,6 +26,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+// 导入底部tabbar路由
+import HomeContainer from '../components/tabbar/HomeContainer';
+import MemberContainen from '../components/tabbar/MemberContainer';
+import SearchContainer from '../components/tabbar/SearchContainer';
+import ShopcarContainer from '../components/tabbar/ShopcarContainer';
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',redirect:'/home'
+    },
+    {
+      path: '/home',
+      component:HomeContainer
+    },
+    {
+      path: '/member',
+      component:MemberContainen
+    },
+    {
+      path: '/search',
+      component:SearchContainer
+    },
+    {
+      path: '/shopcar',
+      component:ShopcarContainer
     }
-  ]
+
+  ],
+  linkActiveClass:'mui-active'//覆盖默认的路由高亮的类
 })
